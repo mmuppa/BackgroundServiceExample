f6bbf9fd21aa63a17dd0b1f4b5983b15572d91c8
mmuppa <mmuppa@uw.edu>
2015-04-26 16:46:51 -0700

    Initial commit

---
+++ app/src/main/java/edu/uw/tacoma/mmuppa/backgroundserviceexample/RSSService.java
@@ -0,0 +1,92 @@
+package edu.uw.tacoma.mmuppa.backgroundserviceexample;
+
+import android.annotation.TargetApi;
+import android.app.IntentService;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+/**
+ * An {@link IntentService} subclass for handling asynchronous task requests in
+ * a service on a separate handler thread.
+ * <p/>
+ * <p/>
+ * helper methods.
+ */
+public class RSSService extends IntentService {
+
+    private static final String CNN_URL = "http://rss.cnn.com/rss/edition.rss";
+    private static final String TAG = "RSSService";
+
+    private int mId = 1;
+
+    public RSSService() {
+        super("RSSService");
+    }
+
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        Log.i(TAG, "Received an Intent: " + intent);
+        String response = "";
+
+        DefaultHttpClient client = new DefaultHttpClient();
+        HttpGet httpGet = new HttpGet(CNN_URL);
+        try {
+            HttpResponse execute = client.execute(httpGet);
+            InputStream content = execute.getEntity().getContent();
+
+            BufferedReader buffer = new BufferedReader(new InputStreamReader(content));
+            String s = "";
+            while ((s = buffer.readLine()) != null) {
+                response += s;
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        Log.i(TAG, response);
+
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(android.R.drawable.bottom_bar)
+                        .setContentTitle("CNN RSS")
+                        .setContentText("RSS Updated");
+        // Creates an explicit intent for an Activity in your app
+        Intent resultIntent = new Intent(this, MainActivity.class);
+
+        // The stack builder object will contain an artificial back stack for the
+        // started Activity.
+        // This ensures that navigating backward from the Activity leads out of
+        // your application to the Home screen.
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        // Adds the back stack for the Intent (but not the Intent itself)
+        stackBuilder.addParentStack(MainActivity.class);
+        // Adds the Intent that starts the Activity to the top of the stack
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent =
+                stackBuilder.getPendingIntent(
+                        0,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
+        mBuilder.setContentIntent(resultPendingIntent);
+        NotificationManager mNotificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        // mId allows you to update the notification later on.
+        mNotificationManager.notify(mId, mBuilder.build());
+    }
+}
